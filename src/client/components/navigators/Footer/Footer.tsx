7e9271369cb7c62ee0dbb44ded66df210c4bf949
sisosyunya <syunyasisotto@gmail.com>
2023-03-05 11:06:34 +0900

    compress avatar

--- src/client/components/navigators/Footer/Footer.tsx
+++ src/client/components/navigators/Footer/Footer.tsx
@@ -28,7 +28,7 @@ export const Footer: FC = () => {
               ))}
             </ul>
             <NavLink to="/">
-              <Image src="/icons/logo.png" />
+              <Image src="/icons/logo.png" loading='lazy'/>
             </NavLink>
           </footer>
         );
